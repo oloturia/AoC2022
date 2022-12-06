0611ef426a5dee0e00d5400f3eb2044a87b48366
oloturia <5429234+oloturia@users.noreply.github.com>
2022-12-06 10:48:22 +0100

    day 6 complete

---
+++ day06/part1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def main(input_file):
+	with open(input_file) as f:
+		input_lines = [line.rstrip() for line in f.readlines()]
+
+	for i in range(0,len(input_lines[0])-2):
+		if len(set(input_lines[0][i:i+4])) == 4:			
+			break
+	return 	i+4
+
+if __name__ == "__main__":
+	test_value = main("TEST1")
+	assert test_value == 7,"Test failed, expected 7, result "+str(test_value)
+	test_value = main("TEST2")
+	assert test_value == 5,"Test failed, expected 5, result "+str(test_value)
+	test_value = main("TEST3")
+	assert test_value == 6,"Test failed, expected 6, result "+str(test_value)
+	test_value = main("TEST4")
+	assert test_value == 10,"Test failed, expected 10, result "+str(test_value)
+	test_value = main("TEST5")
+	assert test_value == 11,"Test failed, expected 11, result "+str(test_value)
+	print(main("INPUT"))
---
+++ day06/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def main(input_file):
+	with open(input_file) as f:
+		input_lines = [line.rstrip() for line in f.readlines()]
+
+	for i in range(0,len(input_lines[0])-12):
+		if len(set(input_lines[0][i:i+14])) == 14:			
+			break
+	return 	i+14
+
+if __name__ == "__main__":
+	test_value = main("TEST1")
+	assert test_value == 19,"Test failed, expected 19, result "+str(test_value)
+	test_value = main("TEST2")
+	assert test_value == 23,"Test failed, expected 23, result "+str(test_value)
+	test_value = main("TEST3")
+	assert test_value == 23,"Test failed, expected 23, result "+str(test_value)
+	test_value = main("TEST4")
+	assert test_value == 29,"Test failed, expected 29, result "+str(test_value)
+	test_value = main("TEST5")
+	assert test_value == 26,"Test failed, expected 26, result "+str(test_value)
+	print(main("INPUT"))
