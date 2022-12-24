a9da10b52ba2918e48e0d4ce76474ce5bf300aaa
oloturia <5429234+oloturia@users.noreply.github.com>
2022-12-24 04:40:49 +0100

    day 20 completed

---
+++ day20/part2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+def main(input_file):
+	with open(input_file) as f:
+		input_lines = [line.rstrip() for line in f.readlines()]
+
+	key = 811589153
+	code_raw = list()
+	code = list()
+	for i,line in enumerate(input_lines):			
+		code.append( {"seq":i, "num": key*int(line) } )
+	
+	for _ in range(10):
+		for seq in range(len(code)):
+			for ind,signal in enumerate(code):
+				if seq == signal["seq"]:
+					move = signal["num"]
+					break
+
+			if move == 0:
+				continue
+			
+			code.remove(signal)
+
+			code.insert((ind+move)%(len(code)),signal)
+
+	
+	for ind,signal in enumerate(code):
+		if signal["num"] == 0:
+			zero = ind
+			break
+	
+	res =  code[(1000+zero)%len(code)]["num"] + code[(2000+zero)%len(code)]["num"] + code[(3000+zero)%len(code)]["num"]
+	
+	
+	return res
+	
+
+
+if __name__ == "__main__":
+	test_value = main("TEST")
+	assert test_value == 1623178306,"Test failed, expected 1623178306, result "+str(test_value)
+	print(main("INPUT"))
